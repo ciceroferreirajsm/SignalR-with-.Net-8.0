10475efea16a0c08de2727e9e59be481ab4e5640
Cicero Ferreira <ciceronascimentu@gmail.com>
2024-10-24 18:45:24 -0300

    Corrigido signalR

--- Hub/ChatHub.cs
+++ Hub/ChatHub.cs
@@ -1,12 +1,12 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 
-namespace SignalRChat.Hubs;
-
-public class ChatHub : Hub
+namespace SignalRChat.Hubs
 {
-    public async Task SendMessage(string user, string message)
+    public class ChatHub : Hub
     {
-        await Clients.All.SendAsync("ReceiveMessage", user, message);
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
     }
 }
-
